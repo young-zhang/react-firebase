5afc2c38e4f29ee305657cf891f39f2405a2620e
Young Zhang <young.quinn.zhang@gmail.com>
2020-02-17 14:33:28 -0500

    add function getScreams; https://youtu.be/m_u6P5k0vP0?t=937

--- socialape-functions/functions/src/index.ts
+++ socialape-functions/functions/src/index.ts
@@ -1,8 +1,23 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+admin.initializeApp(); // app id set in .firebaserc
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
 export const helloWorld = functions.https.onRequest((request, response) => {
     response.send("Hello world!");
+});
+
+export const getScreams = functions.https.onRequest((request, response) => {
+    admin.firestore().collection('screams').get()
+        .then(data => {
+            const screams: FirebaseFirestore.DocumentData[] = [];
+            data.forEach(doc => {
+                screams.push(doc.data());
+            });
+            return response.json(screams);
+        })
+        .catch(err => console.error(err));
 });
