22797ea2d6ed6b103478b884744350977e7cea66
Young Zhang <young.quinn.zhang@gmail.com>
2020-02-17 15:07:56 -0500

    add function createScream; https://youtu.be/m_u6P5k0vP0?t=1248

--- README.md
+++ README.md
@@ -18,3 +18,13 @@ Select `Functions: Configure and deploy Cloud Functions`; `Use an existing proje
 [What is the practice on committing firebase files in a NodeJS app?](https://stackoverflow.com/questions/43527359/what-is-the-practice-on-committing-firebase-files-in-a-nodejs-app)
 
 [12:04](https://youtu.be/m_u6P5k0vP0?t=724) `firebase deploy`
+
+[20:48](https://youtu.be/m_u6P5k0vP0?t=1248) if trying createScream, and you get `Error: Value for argument "data" is not a valid Firestore document.`, 
+make sure the POST request is sending data as JSON and not Text. See https://i.stack.imgur.com/Mb3re.png
+
+[24:43](https://youtu.be/m_u6P5k0vP0?t=1483) 
+go to Firebase Console's *Project Settings*,
+select *Service accounts*, and *Generate new private key*.
+`set GOOGLE_APPLICATION_CREDENTIALS=F:\socialape.json`
+
+`firebase serve` to server the application locally
--- socialape-functions/functions/src/index.ts
+++ socialape-functions/functions/src/index.ts
@@ -1,7 +1,9 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+import firebaseConfig from "../firebaseConfig";
 
-admin.initializeApp(); // app id set in .firebaserc
+//admin.initializeApp(firebaseConfig);
+admin.initializeApp();
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -20,4 +22,24 @@ export const getScreams = functions.https.onRequest((request, response) => {
             return response.json(screams);
         })
         .catch(err => console.error(err));
+});
+
+export const createScream = functions.https.onRequest((req, res) => {
+    console.log("createScream() executing");
+    const {body, userHandle} = req.body;
+    const newScream = {
+        body: body,
+        userHandle: userHandle,
+        createdAt: admin.firestore.Timestamp.fromDate(new Date())
+    };
+    console.error(newScream);
+    admin.firestore().collection('screams')
+        .add(newScream)
+        .then(doc => {
+            res.json({message: `document ${doc.id} created successfully`});
+        })
+        .catch(err => {
+            res.status(500).json({error: 'something went wrong'});
+            console.error(err);
+        });
 });
