d5a4b50954f1505c612f3d4ef7d24d39591fe3ff
Young Zhang <young.quinn.zhang@gmail.com>
2020-02-20 14:16:20 -0500

    add like/unlike

--- socialape-functions/functions/src/handlers/screams.ts
+++ socialape-functions/functions/src/handlers/screams.ts
@@ -2,47 +2,7 @@ import {Request, Response} from "express";
 import {db} from "../util/admin";
 import * as firebase from "firebase";
 import Timestamp = firebase.firestore.Timestamp;
-
-export const getAllScreams = (req: Request, res: Response) => {
-    db.collection("screams")
-        .orderBy("createdAt", "desc")
-        .get()
-        .then(data => {
-            const screams: any = [];
-            data.forEach(doc => {
-                screams.push({
-                    screamId: doc.id,
-                    ...doc.data() // I get to use the ... operator because TypeScript!
-                });
-            });
-            return res.json(screams);
-        })
-        .catch(err => console.error(err));
-};
-
-export const postOneScream = (req: Request, res: Response) => {
-    if (req.method !== "POST") {
-        res.status(400).json({error: "Method not allowed"});
-    }
-    else {
-        const {body} = req.body;
-        const newScream = {
-            body: body,
-            userHandle: req.user["handle"],
-            createdAt: new Date() //.toISOString()
-        };
-        console.error(newScream);
-        db.collection("screams")
-            .add(newScream)
-            .then(doc => {
-                res.json({message: `document ${doc.id} created successfully`});
-            })
-            .catch(err => {
-                res.status(500).json({error: "something went wrong"});
-                console.error(err);
-            });
-    }
-};
+import {object} from "firebase-functions/lib/providers/storage";
 
 interface Comment {
     commentId?: string
@@ -57,9 +17,69 @@ interface Scream {
     screamId?: string
     createdAt?: Timestamp | Date
     userHandle?: string
+    userImage?: string
     body?: string
+    likeCount?: number
+    commentCount?: number
     comments: Comment[]
 }
+
+interface Like {
+    screamId?: string
+    userHandle?: string
+}
+
+export const getAllScreams = (req: Request, res: Response) => {
+    db.collection("screams")
+        .orderBy("createdAt", "desc")
+        .get()
+        .then(data => {
+            const screams: Scream[] = [];
+            data.forEach(doc => {
+                const {body, createdAt, userHandle, imageUrl} = doc.data();
+                let scream: Scream = {
+                    screamId: doc.id,
+                    body,
+                    createdAt: createdAt.toDate(),
+                    userHandle,
+                    userImage: imageUrl,
+                    comments: []
+                };
+                screams.push(scream);
+            });
+            return res.json(screams);
+        })
+        .catch(err => console.error(err));
+};
+
+export const postOneScream = (req: Request, res: Response) => {
+    if (req.method !== "POST") {
+        res.status(400).json({error: "Method not allowed"});
+    }
+    else {
+        const {handle, imageUrl} = req.user;
+        const {body} = req.body;
+        const newScream: Scream = {
+            body: body,
+            userHandle: handle,
+            userImage: imageUrl,
+            createdAt: new Date(),
+            likeCount: 0,
+            commentCount: 0,
+            comments: []
+        };
+        console.error(newScream);
+        db.collection("screams")
+            .add(newScream)
+            .then(doc => {
+                res.json({message: `document ${doc.id} created successfully`});
+            })
+            .catch(err => {
+                res.status(500).json({error: "something went wrong"});
+                console.error(err);
+            });
+    }
+};
 
 export const getScream = (req: Request, res: Response) => {
     let screamData: Scream = {comments: []};
@@ -126,7 +146,7 @@ export const commentOnScream = (req: Request, res: Response) => {
                 return;
             }
         })
-        .then(()=>{
+        .then(() => {
             // doc created successfully
             res.json(newComment);
         })
@@ -137,3 +157,74 @@ export const commentOnScream = (req: Request, res: Response) => {
 
     return;
 };
+
+const updateLikes = (req: Request, res: Response, action: "like" | "unlike") => {
+    const {handle, imageUrl} = req.user;
+    const {screamId} = req.params;
+    const likeDocument = db
+        .collection("likes")
+        .where("userHandle", "==", handle)
+        .where("screamId", "==", screamId)
+        .limit(1);
+    const screamDocument = db.doc(`/screams/${screamId}`);
+    let screamData: Scream = {comments: []};
+    screamDocument
+        .get()
+        .then(doc => {
+            if (doc && doc.exists) {
+                Object.assign(screamData, doc.data());
+                // @ts-ignore
+                screamData.createdAt = doc.data().createdAt.toDate();
+                if (!screamData.likeCount) screamData.likeCount = 0;
+                if (!screamData.commentCount) screamData.commentCount = 0;
+                return likeDocument.get();
+            }
+            else {
+                res.status(404).json({error: "Scream not found"});
+                return;
+            }
+        })
+        .then(data => { // data should contain a like document
+            if (!data) return;
+            let likeCount = (screamData && screamData.likeCount) ? screamData.likeCount : 0;
+            if (action == "like" && data.empty) {
+                db.collection("likes")
+                    .add({screamId, userHandle: handle})
+                    .then(() => {
+                        likeCount++;
+                        return screamDocument.update({likeCount});
+                    })
+                    .then(() => {
+                        screamData.likeCount = likeCount;
+                        res.json(screamData);
+                    });
+            }
+            else if (action == "unlike" && !data.empty) {
+                const {id} = data.docs[0];
+                db.doc(`/likes/${id}`)
+                    .delete()
+                    .then(() => {
+                        if (likeCount > 0) likeCount--;
+                        return screamDocument.update({likeCount});
+                    })
+                    .then(() => {
+                        screamData.likeCount = likeCount;
+                        res.json(screamData);
+                    });
+            }
+            else {
+                if (action === "like")
+                    res.status(400).json({error: "Scream already liked"});
+                else if (action === "unlike")
+                    res.status(400).json({error: "Scream not liked"});
+                return;
+            }
+        })
+        .catch(err => {
+            res.status(500).json({error: err.code});
+        });
+    return;
+};
+
+export const likeScream = (req: Request, res: Response) => updateLikes(req, res, "like");
+export const unlikeScream = (req: Request, res: Response) => updateLikes(req, res, "unlike");
--- socialape-functions/functions/src/index.ts
+++ socialape-functions/functions/src/index.ts
@@ -1,6 +1,6 @@
 import * as functions from "firebase-functions";
 import * as express from "express";
-import {getAllScreams, postOneScream, getScream, commentOnScream} from "./handlers/screams";
+import {getAllScreams, postOneScream, getScream, likeScream, unlikeScream, commentOnScream} from "./handlers/screams";
 import {login, signup, addUserDetails, getAuthenticatedUser, uploadImage} from "./handlers/users";
 import fbAuth from "./util/fbAuth";
 
@@ -11,8 +11,8 @@ app.get("/screams", getAllScreams);
 app.post("/scream", fbAuth, postOneScream);
 app.get("/scream/:screamId", getScream);
 // TODO: delete scream
-// TODO: like scream
-// TODO: unlike scream
+app.get("/scream/:screamId/like", fbAuth, likeScream);
+app.get("/scream/:screamId/unlike", fbAuth, unlikeScream);
 app.post("/scream/:screamId/comment", fbAuth, commentOnScream);
 
 // User routes
