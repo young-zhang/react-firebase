0a8997aab747560671057b7a39d533baa4b99d80
Young Zhang <young.quinn.zhang@gmail.com>
2020-02-21 14:13:41 -0500

    add Navbar

--- socialape-client/src/App.tsx
+++ socialape-client/src/App.tsx
@@ -1,23 +1,27 @@
 import React from 'react';
-import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import './App.css';
 
 import Home from "./pages/home";
 import Login from "./pages/login";
 import Signup from "./pages/signup";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-          <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/login" component={Login} />
-              <Route exact path="/signup" component={Signup} />
-          </Switch>
-      </Router>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Navbar />
+                <div className="container">
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route exact path="/login" component={Login} />
+                        <Route exact path="/signup" component={Signup} />
+                    </Switch>
+                </div>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
--- socialape-client/src/components/Navbar.tsx
+++ socialape-client/src/components/Navbar.tsx
@@ -1,12 +1,18 @@
 import * as React from "react";
 import {Component} from "react";
+import {AppBar, Button, Toolbar} from "@material-ui/core";
+import {Link} from "react-router-dom";
 
 export default class Navbar extends Component {
     render() {
         return (
-            <div>
-                <h1>Home page</h1>
-            </div>
+            <AppBar>
+                <Toolbar className="nav-container">
+                    <Button color="inherit" component={Link} to="/login">Login</Button>
+                    <Button color="inherit" component={Link} to="/">Home</Button>
+                    <Button color="inherit" component={Link} to="/signup">Signup</Button>
+                </Toolbar>
+            </AppBar>
         );
     }
 }
